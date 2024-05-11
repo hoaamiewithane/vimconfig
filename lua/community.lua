47dbf23f9c2b6147b517397446a9d2ef404ebf1e
boy2balls <nguyendanghoaa@gmail.com>
2024-05-11 19:50:32 +0700

    vim set up for webstorm and astrovim

---
+++ lua/community.lua
@@ -0,0 +1,16 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.vscode-nvim"},
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css"},
+  { import = "astrocommunity.completion.copilot-lua"},
+  { import = "astrocommunity.recipes.vscode" }
+}
